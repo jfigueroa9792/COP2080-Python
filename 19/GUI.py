1d069cb8b28bada00fb695272a2b6abc884faf17
Joel Figueroa <jfigueroa9792@floridapoly.edu>
2025-11-03 16:49:56 +0000

    11/3/25

--- 19/GUI.py
+++ 19/GUI.py
@@ -7,11 +7,26 @@
 '''
 def greet():
     name = input_field.value.strip()
-    msg = f"Hello, {name}"
+    msg = f"Hello, {name or "stranger"}"
     ui.notify(msg) # create a popup
 
 input_field = ui.input("Enter your name")
-ui.button("Greet Me!", on_click = greet)
+ui.button("Greet Me!", on_click = greet, color = "green")
+
+
+class State:
+    count = 0
+
+count_label = ui.label("Count: 0")
+
+def add_one():
+    State.count += 1
+    count_label.text = f"Count {State.count}"
+
+ui.button("Add 1", on_click = add_one)
 
 ui.run(title = "Simple Greeting App")
+
+-------------- TODO --------------
+create a reset button which sets the counter to 0
 '''
